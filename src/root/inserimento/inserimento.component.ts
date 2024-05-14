fa7b7e00d96d2e00027505a4e5b8de7bd17a97b8
MatteoBarresi <matteobarresi002@gmail.com>
2024-05-14 16:11:54 +0200

    inserimento quasi completo - manca check duplicati

--- src/root/inserimento/inserimento.component.ts
+++ src/root/inserimento/inserimento.component.ts
@@ -1,4 +1,4 @@
-import { CommonModule, provideImageKitLoader } from '@angular/common';
+import { CommonModule} from '@angular/common';
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { ArchivioService } from '../archivio.service';
 import { Libro } from '../libro';
@@ -20,26 +20,31 @@ export class InserimentoComponent implements OnInit {
   @Output() eventoCambio = new EventEmitter<string>();
  // @Output() eventoCampi = new EventEmitter<string[]>();
 
-  archivio :Archivio;
+  archivio : Archivio = new Archivio([]);
 
   constructor(private as : ArchivioService){}
-  ngOnInit() {}
+  ngOnInit() {this.getCollection()} //archivio scaricato una volta sola, quando si carica la pagina
+  //questo evita anche problemi di timing (es getCollection viene eseguita dopo la set, anche se messa prima nel codice)
   
   togglePaginaInserisci(){
     this.pagina = 'iniziale';
     this.eventoCambio.emit(this.pagina)
   }
 
-  provaGet(){
-    this.archivio = new Archivio([]);
+  getCollection(){
+    //fa la get dal db remoto (ottiene stringa JSON)
+    //crea oggetti libro
+    //li mette in una variabile che è un oggetto di tipo archivo--> serve a fare i controlli
+
     this.as.getData().subscribe(
       {
         next: (x:AjaxResponse<any>)=> {
 
-          console.log(x.response);
-          let foo = (JSON.parse(x.response));
-          foo.map((item)=> this.archivio.add(new Libro(item['autore'],item['titolo'],"","")))
-          console.log(this.archivio);
+          //console.log(x.response);
+          const collezione = (JSON.parse(x.response));
+          collezione.map((item)=> this.archivio.add(new Libro(item['autore'],item['titolo'],item['posizione'],item['nominativo'])))
+          console.log("archivio appena ottenuto " + this.archivio.collezione);
+          this.archivio.collezione.map((item)=> console.log(item));
         },
         error: (err) =>
           console.error('Observer got an error: ' + JSON.stringify(err))
@@ -62,15 +67,17 @@ export class InserimentoComponent implements OnInit {
 
   checkDuplicati(libro:Libro){//riceve il libro appena creato
 
-
-    //fa la get dal db remoto (ottiene stringa JSON)
-    //crea oggetti libro
-    //li mette in una variabile che è un oggetto di tipo archivo--> serve a fare i controlli
-
+    this.archivio;
+    
     //controlla che l'item non ci sia già (o che la posizione non sia occupata)
   }
 
-  send(){ //scarica archivio - check - aggiorna - send
+  send(){ //check duplicati -> aggiorna (aggiunge Libro all'oggetto) -> send
+    
+    //this.getCollection(); //se messa qui, dà problemi
+    
+    //check duplicati
+
     //fare try-catch
     let campi : HTMLCollectionOf<HTMLInputElement>;
     let valori: string[] = [];
@@ -81,25 +88,34 @@ export class InserimentoComponent implements OnInit {
       console.log('check FALLITO');
       //scrivere errore su pagina e non continuare
     }else{
-      Array.from(campi).map((item)=> valori.push(item.value)); //mette le stringhe in un array[]<string>
       
+      //Array.from(campi).map((item)=> valori.push(item.value)); //mette le stringhe in un array[]<string>
+      //new Libro(valori[0],valori[1],valori[2],valori[3])
+
       //crea libro
-      const libro = new Libro(valori[0],valori[1],valori[2],valori[3]);
+      const libro = new Libro(
+        campi['campoAutore'].value, 
+        campi['campoTitolo'].value, 
+        campi['campoPosizione'].value, 
+        campi['campoNominativo'].value
+        );
       
-      //va istanziato prima
-      this.archivio = new Archivio([libro]);
-      console.log(this.archivio.collezione);
       //checkDuplicati(libro);
-  
       
-      //valori.map((item)=> console.log(JSON.stringify(valori)));
+      this.archivio.add(libro);
       
+      console.log("archivio aggiornato " + this.archivio.collezione);
+      
+      //trasforma oggetto in stringa e fa la set
       this.as.setData(JSON.stringify(this.archivio.collezione)).subscribe({
-        next: (res: AjaxResponse<any>)=> console.log(res.response),
+        next: (res: AjaxResponse<any>)=> {
+          console.log(res.response); 
+          console.log("stringa mandata " + JSON.stringify(this.archivio.collezione));
+        },
         error: (err: AjaxError)=> console.log(err)
       })
-    }
-    
+    };
+ 
   }
 
 }
